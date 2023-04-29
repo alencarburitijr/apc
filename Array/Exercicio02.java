197890bcda0981c36739db2960d56b61e9d342fa
Alencar Buriti <alencarburitijr@gmail.com>
2023-04-28 21:00:20 -0300

    AULA

---
+++ Array/Exercicio02.java
@@ -0,0 +1,13 @@
+package Array;
+/* Crie um programa que leia 20 números inteiros e
+verifique se é impar ou par. 
+Adicione cada número em um array específico para
+impar e outro para par. Ao final, imprima os valores
+
+Com o array de inteiros criado, crie um novo 
+arry e insira os números pares e depois os números 
+impares. Imprima o array resultante.
+*/
+public class Exercicio02 {
+    
+}
